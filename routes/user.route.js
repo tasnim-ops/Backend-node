1643e7d64ae41c73a0d4deef00b63e8495548b8c
tasnim <hajji.tas520@gmail.com>
2023-05-04 16:38:27 +0100

    Initial commit

---
+++ routes/user.route.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const router = express.Router();
+const User=require("../models/user")
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const nodemailer=require('nodemailer');
+var transporter =nodemailer.createTransport({
+service:'gmail',
+auth:{
+user:'esps421@gmail.com',
+pass:'ptvprsmkuhoizqbe'
+},
+tls:{
+rejectUnauthorized:false
+}
+})
+require('dotenv').config()
+
+
+// créer un nouvel utilisateur
+router.post('/register', async (req, res) => {
+    try {
+    let { email, password, firstname, lastname,avatar } = req.body
+    const user = await User.findOne({ email })
+    if (user) return res.status(404).send({ success: false, message:"User already exists" })
+    const newUser = new User({ email, password, firstname, lastname,avatar })
+    const createdUser = await newUser.save()
+
+    // Envoyer l'e-mail de confirmation de l'inscription
+var mailOption ={
+    from: '"verify your email " <hajji.tas520@gmail.com>',
+    to: newUser.email,
+    subject: 'vérification your email ',
+    html:`<h2>${newUser.firstname}! thank you for registreting on our
+    website</h2>
+    <h4>please verify your email to procced.. </h4>`
+}
+    transporter.sendMail(mailOption,function(error,info){
+    if(error){
+    console.log(error)
+    }
+    else{
+    console.log('verification email sent to your gmail account ')
+    }
+    })
+
+
+
+    return res.status(201).send({ success: true, message: "Account created successfully", user: createdUser })
+    } catch (err) {
+    console.log(err)
+    res.status(404).send({ success: false, message: err })
+    }
+    });
+
+
+    // afficher la liste des utilisateurs.
+router.get('/', async (req, res, )=> {
+    try {
+    const users = await User.find().select("-password").select("-lastname");
+    res.status(200).json(users);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    });
+/**
+* as an admin i can disable or enable an account
+*/
+router.put('/status/edit', async (req, res) => {
+    try {
+        let email = req.query.email
+        console.log(email)
+        let user = await User.findOne({email})
+        user.isActive = !user.isActive
+        user.save()
+        res.status(200).send({ success: true, user })
+        } catch (err) {
+        return res.status(404).send({ success: false, message: err })
+        }        
+    })
+
+// afficher la liste des utilisateurs.
+router.get('/', async (req, res, )=> {
+    try {
+    const users = await User.find().select("-password");
+    res.status(200).json(users);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    });
+    
+    /**
+* as an admin i can disable or enable an account 
+*/
+router.put('/status/edit', async (req, res) => {
+    try {
+    let { idUser } = req.body
+    let user = await User.findById(idUser).select('+isActive')
+    user.isActive = !user.isActive
+    user.save()
+    res.status(200).send({ success: true, user })
+    } catch (err) {
+    return res.status(404).send({ success: false, message: err })
+    }
+    })
+
+    // se connecter
+router.post('/login', async (req, res) => {
+    try {
+    let { email, password } = req.body
+    if (!email || !password) {
+        return res.status(404).send({ success: false, message: "All fields are required" })
+        }
+        let user = await User.findOne({ email
+        }).select('+password').select('+isActive')
+        if (!user) {
+        return res.status(404).send({ success: false, message: "Account doesn't exists" })
+        } else {
+        let isCorrectPassword = await bcrypt.compare(password, user.password)
+        if (isCorrectPassword) {
+        delete user._doc.password
+        if (!user.isActive) return res.status(200).send({ success:
+        false, message: 'Your account is inactive, Please contact your administrator' })
+        const token = jwt.sign({ iduser: user._id, role: user.role
+        }, process.env.SECRET, { expiresIn: "1h", })
+        return res.status(200).send({ success: true, user, token })
+        } else {
+            return res.status(404).send({ success: false, message:
+                "Please verify your credentials" })
+                } }
+        } catch (err) {
+        return res.status(404).send({ success: false, message: err.message
+        })
+        }
+        });
+    
+    module.exports = router;
